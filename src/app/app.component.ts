182a73bb230af7c4698dbddf0c6071624dbda982
jayakrishnaenterpi <t.jayakrishna@enterpi.com>
2018-03-19 18:11:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  firstname;
+  lastname;
+  private restAPI = 'https://jsonplaceholder.typicode.com/posts';
   title = 'app';
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    console.log('firstname: ' + this.firstname);
+    console.log('lastname: ' + this.lastname);
+    const payload = {firstname : this.firstname, lastname: this.lastname};
+    this.http.post(this.restAPI, payload).toPromise().then((response) => {
+      console.log(response);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule, MatButtonModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
